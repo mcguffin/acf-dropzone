ce62a3bd79ffbdf9bf181794bef247bdc068ea52
mcguffin <joern@flyingletters.com>
2019-04-09 13:21:12 +0200

    Release version 0.1.5

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin Name: ACF Dropzone
 Plugin URI: http://wordpress.org/
 Description: Enter description here.
 Author: Jörn Lund
-Version: 0.1.4
+Version: 0.1.5
 Author URI: https://github.com/mcguffin
 License: GPL3
 Github Repository: mcguffin/acf-dropzone
