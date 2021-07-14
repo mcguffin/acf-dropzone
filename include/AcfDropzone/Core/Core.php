75157ccce8fe0f1f5444e7d18da237a23deddc08
mcguffin <joern@flyingletters.com>
2021-07-14 13:31:56 +0200

    cleanup core class

--- include/AcfDropzone/Core/Core.php
+++ include/AcfDropzone/Core/Core.php
@@ -21,20 +21,9 @@ class Core extends Plugin implements CoreInterface {
 	protected function __construct() {
 
 		add_action( 'plugins_loaded' , array( $this , 'init_compat' ), 0 );
-		add_action( 'init' , array( $this , 'init' ) );
-
-		add_action( 'wp_enqueue_scripts' , array( $this , 'enqueue_assets' ) );
 
 		$args = func_get_args();
 		parent::__construct( ...$args );
-	}
-
-	/**
-	 *	Load frontend styles and scripts
-	 *
-	 *	@action wp_enqueue_scripts
-	 */
-	public function enqueue_assets() {
 	}
 
 
@@ -49,17 +38,5 @@ class Core extends Plugin implements CoreInterface {
 			Compat\ACF::instance();
 		}
 	}
-
-
-
-
-	/**
-	 *	Init hook.
-	 *
-	 *  @action init
-	 */
-	public function init() {
-	}
-
 
 }
